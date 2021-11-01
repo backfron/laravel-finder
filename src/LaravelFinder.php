0cc8f754091353795b076b01951f2f86d6bab5a8
Jairo Ushiña <jago86@gmail.com>
2021-11-01 16:53:29 -0500

    Can apply global filters.

--- src/LaravelFinder.php
+++ src/LaravelFinder.php
@@ -2,10 +2,11 @@
 
 namespace Backfron\LaravelFinder;
 
+use Closure;
+use BadMethodCallException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Backfron\LaravelFinder\Exceptions\FilterNotFoundException;
-use BadMethodCallException;
 
 class LaravelFinder
 {
@@ -13,10 +14,13 @@ class LaravelFinder
 
     protected $filters = [];
 
+    protected $globalFilters = [];
+
     protected $query;
 
     protected static $swapMethods = [
         'filters' => 'addFilters',
+        'global' => 'addGlobalFilters',
     ];
 
     public function __construct()
@@ -25,10 +29,10 @@ class LaravelFinder
     }
 
     /**
-     * Apply filters
+     * Add filters
      *
      * @param array $filters
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @return self
      */
     public function addFilters(array|string $filters, $value = null)
     {
@@ -39,6 +43,22 @@ class LaravelFinder
         } else {
             $this->filters = $filters;
         }
+
+        return $this;
+    }
+
+    /**
+     * Add global filters
+     *
+     * @param Closure $filters
+     * @return self
+     */
+    public function addGlobalFilters(Closure|array $filters)
+    {
+        if (!is_array($filters)) {
+            $filters = (array) $filters;
+        }
+        $this->globalFilters = $filters;
 
         return $this;
     }
@@ -63,7 +83,29 @@ class LaravelFinder
                 throw new FilterNotFoundException;
             }
             if (class_exists($filterClassName)) {
-                $this->query = $filterClassName::apply($this->query, $filterValue);
+                $filterClassName::apply($this->query, $filterValue);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Applies global filters to the query
+     *
+     * @return self
+     */
+    protected function applyGlobalFilters()
+    {
+        foreach ($this->globalFilters as $filter) {
+
+            if ($filter instanceof Closure) {
+                $filter($this->query);
+            }
+
+            if (is_array($filter)) {
+                $filterClass = new $filter[0];
+                $filterClass::apply($this->query, $filter[1]);
             }
         }
 
@@ -89,21 +131,33 @@ class LaravelFinder
 
     public static function __callStatic($method, $parameters)
     {
-        $method = static::$swapMethods[$method];
-        return (new static)->$method(...$parameters);
+        if (array_key_exists($method, static::$swapMethods)) {
+            $method = static::$swapMethods[$method];
+            return (new static)->$method(...$parameters);
+        }
+
+        $message = 'Method "%s" not found.';
+
+        throw new BadMethodCallException(sprintf($message, $method));
     }
 
     public function __call($method, $parameters)
     {
         if (method_exists($this->model, $method) OR method_exists(QueryBuilder::class, $method)) {
 
-            $this->applyFilters();
+            $this->applyFilters()
+                ->applyGlobalFilters();
 
             if (empty($parameters)) {
                 return call_user_func([$this->query, $method]);
             }
 
             return call_user_func([$this->query, $method], ...$parameters);
+        }
+
+        if (array_key_exists($method, static::$swapMethods)) {
+            $method = static::$swapMethods[$method];
+            return $this->$method(...$parameters);
         }
 
         $message = 'Method "%s" not found.';
--- tests/Feature/FinderTest.php
+++ tests/Feature/FinderTest.php
@@ -2,6 +2,7 @@
 
 namespace Backfron\LaravelFinder\Feature\Tests;
 
+use Backfron\LaravelFinder\Finders\Posts\Filters\FooTitle;
 use Illuminate\Support\Facades\Config;
 use Backfron\LaravelFinder\Finders\Posts\FooPostsFinder;
 use Backfron\LaravelFinder\Tests\LaravelFinderTestCase;
@@ -29,6 +30,45 @@ class FinderTest extends LaravelFinderTestCase
         $this->assertCount(1, $posts);
         $this->assertEquals(6, $posts[0]->id);
         $this->assertEquals(1, $posts[0]->user_id);
+    }
+
+    /** @test */
+    public function test_can_apply_a_global_filter()
+    {
+        $posts = FooPostsFinder::global(fn ($query) => $query->where('user_id', 1))
+        ->filters([
+            'foo_title' => 'Vue.js',
+        ])->get();
+
+        $this->assertCount(1, $posts);
+        $this->assertEquals(6, $posts[0]->id);
+        $this->assertEquals(1, $posts[0]->user_id);
+    }
+
+    /** @test */
+    public function test_can_apply_many_global_filters()
+    {
+        $posts = FooPostsFinder::global([
+            fn ($query) => $query->where('title', 'LIKE', '%laravel%'),
+            fn ($query) => $query->where('title', 'LIKE', '%vue%'),
+        ])->filters([])->get();
+
+        $this->assertCount(1, $posts);
+        $this->assertEquals(6, $posts[0]->id);
+        $this->assertEquals(1, $posts[0]->user_id);
+    }
+
+    /** @test */
+    public function test_can_apply_class_filters_as_global_filters()
+    {
+        $posts = FooPostsFinder::global([
+            [FooTitle::class, 'Vue.js'],
+        ])->filters([])->get();
+
+        $this->assertCount(3, $posts);
+        $this->assertEquals(2, $posts[0]->id);
+        $this->assertEquals(5, $posts[1]->id);
+        $this->assertEquals(6, $posts[2]->id);
     }
 
     /** @test */
